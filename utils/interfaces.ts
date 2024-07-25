255d07e9c056693cccadddc8492c829a4cf2b690
Mateus Ceci <mateusceci@gmail.com>
2024-07-25 19:24:42 -0300

    create interfaces

---
+++ utils/interfaces.ts
@@ -0,0 +1,35 @@
+export interface User {
+  id: string;
+  email: string;
+  meetings: Meeting[];
+}
+
+export interface Guest {
+  id: string;
+  email: string;
+  meetingId: string;
+  meeting: Meeting;
+}
+
+export interface Meeting {
+  id: string;
+  title: string;
+  description?: string;
+  startTime: Date;
+  endTime: Date;
+  userId: string;
+  guests: {
+    create: Omit<Guest, "id" | "meetingId" | "meeting">[];
+  }
+  summary?: string;
+}
+
+export interface MeetingForm {
+  title: string;
+  description?: string;
+  startTime: Date;
+  endTime: Date;
+  guests: {
+    create: Omit<Guest, "id" | "meetingId" | "meeting">[];
+  };
+}

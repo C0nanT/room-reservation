aa279cb18eab64f9ceee30594093cf1ebbb609e1
Mateus Ceci <mateusceci@gmail.com>
2024-07-26 13:17:29 -0300

    create sidenav

---
+++ components/Sidenav.tsx
@@ -0,0 +1,14 @@
+const Sidenav = () => {
+  return (
+    <aside className="h-screen w-64 bg-muted px-2 py-16">
+      <ul>
+        <li>Home</li>
+        <li>Individual Room</li>
+        <li>Meeting Room</li>
+        <li>Events Room</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidenav;

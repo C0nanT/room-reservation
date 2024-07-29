ac4a6b7019a04c65e74d28c02dc7bb7a131f8e2c
Mateus Ceci <mateusceci@gmail.com>
2024-07-29 14:03:43 -0300

    add topbar

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -2,23 +2,36 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import { ModeToggle } from "./ModeToggle";
+import { Button } from "./ui/button";
+import Topnav from "./Topnav";
 
-const TopNav = () => {
+const Topbar = () => {
   const { isSignedIn } = useUser();
 
   return (
-    <div className="flex w-full justify-between bg-muted px-16 py-4">
+    <div className="flex w-full justify-between bg-transparent md:bg-muted px-4 md:px-16 py-4">
       <div className="flex items-center gap-4">
-        <Image src="/logo.png" alt="Dream Room" width={100} height={100} className="h-10 w-10" />
-        <h1 className="text-3xl">Dream Room</h1>
+        <Image
+          src="/logo.png"
+          alt="Dream Room"
+          width={100}
+          height={100}
+          className="h-10 w-10"
+        />
+        <div className="block md:hidden">
+          <Topnav />
+        </div>
+        <h1 className="hidden md:block text-3xl">Dream Room</h1>
       </div>
 
       <div className="flex items-center gap-6">
-        {isSignedIn ? <SignOutButton /> : <SignInButton />}
+        <Button asChild>
+          {isSignedIn ? <SignOutButton /> : <SignInButton />}
+        </Button>
         <ModeToggle />
       </div>
     </div>
   );
 };
 
-export default TopNav;
+export default Topbar;

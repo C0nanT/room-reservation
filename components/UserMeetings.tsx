ef1af7a67f075ead7585b6a8984c70eedc799c73
Mateus Ceci <mateusceci@gmail.com>
2024-08-08 20:16:40 -0300

    teste

--- components/UserMeetings.tsx
+++ components/UserMeetings.tsx
@@ -37,7 +37,7 @@ const UserMeetings = () => {
 
   return (
     <div className="w-full px-4">
-      <h1 className="py-12 text-center text-4xl">My Reservations</h1>
+      <h1 className="py-12 text-center text-4xl">My Reservations a</h1>
       <div className="flex flex-col items-center lg:block mb-4 max-w-[1500px] mx-auto">
         <h2 className="mb-2 text-3xl">Reservations</h2>
         <div className="grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-2 ">
